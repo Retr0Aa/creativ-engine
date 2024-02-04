3785f796e2af8b64899a9f5b3c7a725238045899
Alexander Buchkov <retr0aalex@gmail.com>
2024-02-04 17:12:42 +0200

    Added Premake Support

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+workspace "CreativEngine"
+    configurations { "Debug", "Release" }
+    startproject "CreativEditor"
+
+    group "Engine"
+    project "CreativEngine"
+        kind "SharedLib"
+        language "C#"
+        targetdir "bin/Engine/%{cfg.buildcfg}"
+        location "CreativEngine"
+
+        files { "CreativEngine/src/**.cs" }
+
+        links { "Retr0Log", "System.Drawing", "System.Windows.Forms" }
+    
+    project "CreativEditor"
+        kind "ConsoleApp"
+        location "CreativEditor"
+        language "C#"
+        targetdir "bin/Editor/%{cfg.buildcfg}"
+
+        files { "CreativEditor/src/**.cs" }
+
+        links { "System", "CreativEngine", "System.Drawing", "System.Windows.Forms" }
+
+    group ""
+    project "BuildGame"
+        kind "ConsoleApp"
+        language "C#"
+        targetdir "bin/BuildGame/%{cfg.buildcfg}"
+        location "BuildGame"
+    
+        files { "BuildGame/src/**.cs" }
+    
+        links { "CreativEngine", "System.Drawing", "System.Windows.Forms" }
+
+    group "Dependecies"
+    include "Dependencies/Retr0Log"
